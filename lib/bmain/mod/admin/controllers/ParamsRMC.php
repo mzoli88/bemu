874b686b818fe1b22893d8e760ce78d43d7c52a2
Megyik <megyik.zoltan@hwstudio.hu>
2024-03-07 12:51:32 +0100

    +

--- lib/bmain/mod/admin/controllers/ParamsRMC.php
+++ lib/bmain/mod/admin/controllers/ParamsRMC.php
@@ -3,11 +3,8 @@
 namespace mod\admin\controllers;
 
 use hws\rmc\Controller3;
-use hws\UploadCheck;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Storage;
-use mod\admin\models\Params;
 
 class ParamsRMC extends Controller3
 {
@@ -31,19 +28,9 @@ class ParamsRMC extends Controller3
         Cache::forget('log_debug');
 
         $params = request()->all();
-        // if($request->filled('login_logo') && is_array($request->login_logo)){
-        //     $params['login_logo'] = 'SiteLogo.'. preg_replace('/.*\./','', $request->login_logo['name']);
-        //     UploadCheck::runFromJson($request->login_logo,[
-        //         'image/jpeg' => ['jpeg', 'jpg', 'jpe'],
-        //         'image/png' => ['png'],
-        //         'image/gif' => ['gif'],
-        //     ]);
-        //     // $file = new File('public/'.$params['login_logo'], base64_decode($request->login_logo["file_content"]));
-        //     Storage::put('public/'.$params['login_logo'], base64_decode($request->login_logo["file_content"]));
-        // }
+
         setParams($params);
         
-        // Params::cacheBorderConfig();
         return [];
     }
 }
