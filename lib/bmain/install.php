cb673dd241c48214d938008bb45ca8c79de867c6
Megyik <megyik.zoltan@hwstudio.hu>
2024-04-29 11:21:08 +0200

    +javítások

--- lib/bmain/install.php
+++ lib/bmain/install.php
@@ -2,7 +2,10 @@
 
 $output = shell_exec('composer install');
 $output = preg_replace('#\\x1b[[][^A-Za-z]*[A-Za-z]#', '', $output);
-
 echo "<pre>$output</pre>";
+
 $output = shell_exec('php artisan migrate --force');
+echo "<pre>$output</pre>";
+
+$output = shell_exec('php artisan optimize:clear');
 echo "<pre>$output</pre>";
