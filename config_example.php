f0b6f83237aacdd4fd6c6e451c0174bff1fabd78
Megyik <megyik.zoltan@hwstudio.hu>
2024-05-03 08:13:05 +0200

    +example

---
+++ config_example.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Új telepítéskor, törölni kel a "new_" a config.php elejéről és a htaccess elejéről is. (oda pont is kell)
+
+*/
+
+/**
+* Globalis adatbazis parameterek
+*/
+define('BORDER_DB_HOST', 'localhost');
+define('BORDER_DB_USER', 'root');
+define('BORDER_DB_PASSWORD', '');
+define('BORDER_DB_DATABASE', 'border');
+
