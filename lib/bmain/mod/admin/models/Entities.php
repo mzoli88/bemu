a5e85f22ae77e773529d0b841d160160847b6edd
Megyik <megyik.zoltan@hwstudio.hu>
2024-06-20 08:07:26 +0200

    +

--- lib/bmain/mod/admin/models/Entities.php
+++ lib/bmain/mod/admin/models/Entities.php
@@ -12,67 +12,101 @@ class Entities extends Model
 
     public $timestamps = false;
 
-    public function defaultCollection(){
+    public function defaultCollection()
+    {
         return $this->toArray();
     }
 
     protected $fillable = [
-		"name",
+        "name",
         "status",
     ];
 
     protected $casts = [
-		"id" => "integer",
+        "id" => "integer",
         "name" => "string",
         "status" => "string",
     ];
 
     protected $validation = [
-		"name" => "required|max:255",
+        "name" => "required|max:255",
         "status" => "required|max:1",
     ];
 
     protected $labels = [
-		"id" => "Azonosító",
+        "id" => "Azonosító",
         "name" => "Entitás",
         "status" => "Státusz",
     ];
 
-    public function Rel_Groups_entity(){
-        return $this->hasMany(Groups::class,"entity_id","id");
+    /* ->byPerms(['jog1','jog']) */
+    public function scopeByPerms($q, array $perms, $user_id = null, $modul_azon = null)
+    {
+        if(config('isBorder')) return $q;
+        $modul_azon = $modul_azon ?: getModulAzon();
+        $user_id = $user_id ?: getUserId();
+
+        $q->orWhereIn('admin_entities.id', function ($q) use ($modul_azon, $user_id, $perms) {
+            $q->select('entity_id')
+                ->from('admin_view_perms');
+
+            if (is_array($perms)) $q->whereIn('perm', $perms);
+            else $q->where('perm', $perms);
+
+            if (is_array($modul_azon)) $q->whereIn('modul_azon', $modul_azon);
+            else $q->where('modul_azon', $modul_azon);
+
+            if (is_array($user_id)) $q->whereIn('user_id', $user_id);
+            else $q->where('user_id', $user_id);
+
+        });
+
+        return $q;
     }
 
-    public function Rel_GroupPerms_entity(){
-        return $this->hasMany(GroupPerms::class,"entity_id","id");
+    public function Rel_Groups_entity()
+    {
+        return $this->hasMany(Groups::class, "entity_id", "id");
     }
 
-    public function Rel_UserEntities_entity(){
-        return $this->hasMany(UserEntities::class,"entity_id","id");
+    public function Rel_GroupPerms_entity()
+    {
+        return $this->hasMany(GroupPerms::class, "entity_id", "id");
     }
 
-    public function Rel_UserPerms_entity(){
-        return $this->hasMany(UserPerms::class,"entity_id","id");
+    public function Rel_UserEntities_entity()
+    {
+        return $this->hasMany(UserEntities::class, "entity_id", "id");
+    }
+
+    public function Rel_UserPerms_entity()
+    {
+        return $this->hasMany(UserPerms::class, "entity_id", "id");
     }
 
 
-	public function Rel_Emails_entity(){
-        return $this->hasMany(Emails::class,"entity_id","id");
+    public function Rel_Emails_entity()
+    {
+        return $this->hasMany(Emails::class, "entity_id", "id");
     }
 
-	public function Rel_Notifications_entity(){
-        return $this->hasMany(Notifications::class,"entity_id","id");
+    public function Rel_Notifications_entity()
+    {
+        return $this->hasMany(Notifications::class, "entity_id", "id");
     }
 
-	public function Rel_TemplateMessages_entity(){
-        return $this->hasMany(TemplateMessages::class,"entity_id","id");
+    public function Rel_TemplateMessages_entity()
+    {
+        return $this->hasMany(TemplateMessages::class, "entity_id", "id");
     }
 
-	public function Rel_NotifyEmails_entity(){
-        return $this->hasMany(NotifyEmails::class,"entity_id","id");
+    public function Rel_NotifyEmails_entity()
+    {
+        return $this->hasMany(NotifyEmails::class, "entity_id", "id");
     }
 
-	public function Rel_NotifyPushs_entity(){
-        return $this->hasMany(NotifyPushs::class,"entity_id","id");
+    public function Rel_NotifyPushs_entity()
+    {
+        return $this->hasMany(NotifyPushs::class, "entity_id", "id");
     }
-
 }
